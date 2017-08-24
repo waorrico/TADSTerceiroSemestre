030e57cf1d96f6053dfefdec87c1cfb9c327b3a3
aluno <aluno@estacio.br>
2017-08-24 20:51:13 -0300

    Exercicio aula 4

---
+++ src/br/estacio/programacao/PrimeiraClasse.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.programacao;
+
+/**
+ *
+ * @author aluno
+ */
+public class PrimeiraClasse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //variavel do tipo logico recebe os valores true e false
+        boolean h = true;
+        // variavel do tipo caracter
+        char b = 'x';
+        // variavel do tipo inteiro  
+        byte asd = 100;
+        long y = 12345;
+        int x = 10;
+        short aaa = 4516;
+        //variavel do tipo ponto flutuantr / real
+        float z = 125.45218f;
+        double xyz = 1234.2545;
+        //obejto do tipo Palavra
+        String palavra = "Sou uma string";
+
+        System.out.println("Variavel do Tipo boolean h(" + h + ")");
+        System.out.println("Variavel do Tipo char b(" + b + ")");
+        System.out.println("Variavel do Tipo int x(" + x + ")");
+        System.out.println("Variavel do Tipo double xyz(" + xyz + ")");
+        System.out.println("Variavel do Tipo long y(" + y + ")");
+        System.out.println("Variavel do Tipo float z(" + z + ")");
+        System.out.println("Variavel do Tipo byte asd(" + asd + ")");
+        System.out.println("Variavel do Tipo sort aaa(" + aaa + ")");
+        System.out.println("Objeto do tipo String (" + palavra + ")");
+
+    }
+
+}
