045088dc83c1bd75649deacbebd5b9ec666fe36f
aluno <aluno@estacio.br>
2017-08-30 23:10:47 -0300

    exercicio de variaveis

---
+++ src/br/estacio/programacao/ExercicioVariaveis.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.programacao;
+
+/**
+ *
+ * @author aluno
+ */
+public class ExercicioVariaveis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int idade = 47;
+        String nome = "Waldir";
+        double peso = 94.700;
+        char  rh = '+';
+        String datan = "09/04/1960";
+        int nsapato = 40;
+        String casado = "Sim";
+        
+    }
+    
+}
---
+++ src/br/estacio/programacao/TesteMultiplicacaoIntFloat.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.programacao;
+
+/**
+ *
+ * @author aluno
+ */
+public class TesteMultiplicacaoIntFloat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x =2;
+        float y = 3.1f;
+        float z = x * y;
+        
+        System.out.println("z");
+                
+    }
+    
+}
