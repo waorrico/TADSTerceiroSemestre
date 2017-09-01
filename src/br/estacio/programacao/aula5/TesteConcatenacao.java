25f13486aef6513aa9636e8b63bbab1f83c9c6f1
aluno <aluno@MSCGRESL04023.estacio.acad>
2017-08-31 20:15:00 -0400

    Teste de Concatenação

---
+++ src/br/estacio/programacao/aula5/TesteConcatenacao.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.programacao.aula5;
+
+/**
+ *
+ * @author aluno
+ */
+public class TesteConcatenacao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    String a = "String";
+    int b = 3;
+    int c = 7;
+    int d = 50;
+    System.out.println(b + c + d + a);
+    System.out.println(b + c + a + d);
+    System.out.println(a + b + c + d);
+    }
+    
+}
